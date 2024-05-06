d650bd585f8d49845de4e23c1490f78f5e85ff8c
SE-63\Batch 10 <egaoktabrianto@gmail.com>
2024-05-06 14:59:57 +0700

    Project A and B

---
+++ Project-A/Program.cs
@@ -0,0 +1,33 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// class Program{
+//     static void Main(){
+//         Cat caty = new Cat();
+//         caty.Eat();
+//     }
+// }
+
+// class Cat{
+//     int age;
+//     string color;
+//     string species;
+
+//     public void Eat(){
+//         Console.WriteLine("meong");
+//     }
+// }
+
+public class Solution {
+    public int MySqrt(int x) {
+        double i=0;
+        while(i*i<=x)
+        {
+            
+            if( (i*i) <= x && (i+1)*(i+1) > x)
+                return (int)i;
+                i++;
+        }
+        return 0;
+    }
+}
---
+++ ProjectB/Program.cs
@@ -0,0 +1,49 @@
+﻿class Employee{
+    string name;
+    string occupation;
+    string city;
+    int salary;
+    int experience;
+
+    public void printName(){
+        Console.WriteLine("Input nama: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Aku seorang " + name);
+    }
+
+    public void printOccupation(){
+        Console.WriteLine("Input pekerjaan: ");
+        occupation = Console.ReadLine();
+        Console.WriteLine("Pekerjaan ku " + occupation);
+
+    }
+
+    public void printCity(){
+        Console.WriteLine("Input kota: ");
+        city = Console.ReadLine();
+        Console.WriteLine("Aku tinggal di " + city);
+    }
+
+    public void printSalary(){
+        Console.WriteLine("Input gaji: ");
+        int salary = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Gaji ku Rp." + salary);
+    }
+
+    public void printExperience(){
+        Console.WriteLine("Input pengalaman: ");
+        int experience = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Telah bekerja selama " + experience + " Tahun");
+    }
+}
+
+class Program{
+    static void Main(){
+        Employee emp = new Employee();
+        emp.printName();
+        emp.printOccupation();
+        emp.printCity();
+        emp.printSalary();
+        emp.printExperience();
+    }
+}
