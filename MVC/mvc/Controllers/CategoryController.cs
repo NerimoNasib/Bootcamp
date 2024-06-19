ebb41d04ef51e1a85132def0acf33956e112103f
NerimoNasib <egainsanity@gmail.com>
2024-06-19 09:02:38 +0700

    MVC CRUD

--- MVC/mvc/Controllers/CategoryController.cs
+++ MVC/mvc/Controllers/CategoryController.cs
@@ -1,3 +1,4 @@
+using System.Data.Common;
 using Microsoft.AspNetCore.Mvc;
 
 public class CategoryController : Controller{
@@ -9,5 +10,35 @@ public class CategoryController : Controller{
     public IActionResult Index(){
         List<Category> categories = _db.Categories.ToList();
         return View(categories);
+    }
+
+    public IActionResult Create(){
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Category category){
+        _db.Categories.Add(category);
+        await _db.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Edit(int? id){
+        if(id == null || id == 0) return NotFound();
+        Category category = _db.Categories.Find(id);
+        return View(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Category category){
+        Category existingCategory = _db.Categories.Find(category.CategoryID);
+        existingCategory.CategoryName = category.CategoryName;
+        existingCategory.Description = category.Description;
+        await _db.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Delete(){
+        return View();
     }
 }
--- MVC/mvc/obj/Debug/net8.0/mvc.AssemblyInfo.cs
+++ MVC/mvc/obj/Debug/net8.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d52e0ca2d15e65db894154462b893f6fb9e4026")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c340b4a2c4337b12c44d83347d346ec52f7d4f0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
